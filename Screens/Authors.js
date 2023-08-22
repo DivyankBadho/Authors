1c4f60dc6c57af8c13d974a49fbde44bdd325c58
DivyankBadho <divyank.garg@badho.in>
2023-08-22 15:08:33 +0530

    feat: add-authors form

---
+++ Screens/Authors.js
@@ -0,0 +1,141 @@
+import React,{useEffect} from "react";
+import { View,Text,StyleSheet, FlatList,TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+// import { useNavigationOptions } from "react-native-navigation-hooks";
+import { useQuery, gql } from '@apollo/client';
+
+const MY_QUERY=gql`
+query MyQuery{
+    author_author {
+      name
+      id
+      books {
+        name
+        id
+      }
+      books_aggregate {
+        aggregate {
+          count
+        }
+      }
+    }
+  }
+  
+ 
+`
+
+const Authors=()=>{
+
+    const{data, loading,refetch} = useQuery(MY_QUERY)
+    const navigation = useNavigation();
+
+    useEffect(() => {
+      // Refetch data whenever this component is focused
+      const unsubscribe = navigation.addListener("focus", () => {
+        refetch();
+      });
+  
+      return unsubscribe;
+    }, [navigation, refetch]);
+
+    const navigateToBooks = (author) => {
+        navigation.navigate("Books", { author });
+        // console.log(id)
+      };
+
+     
+
+  useEffect(()=>{
+    console.log('GraphQL ===', data.author_author[0])
+  })
+    return(
+        
+        <View style={styles.container}>
+            <FlatList
+            data={data?.author_author}
+            renderItem={({item})=>(
+     <TouchableOpacity onPress={() => navigateToBooks(item)}>
+      <View style={styles.card}>
+        <View style={styles.description}>
+          <Text style={{ fontSize: 20, fontWeight: "bold" ,marginLeft:20}}>
+            Author: {item.name}
+          </Text>
+          <View style={styles.roundBoundary}>
+          <Text style={styles.roundText}>
+            {item.books_aggregate.aggregate.count}
+          </Text>
+          </View>
+        </View>
+        <Text style={{ marginLeft: 70, fontSize: 15 }}> ID:{item.id}</Text>
+      </View>
+      </TouchableOpacity>
+     )}
+       />
+        {/* <TouchableOpacity style={styles.addButton} onPress={navigateToaddAuthor}>
+        <Text style={styles.addButtonText}>ADD</Text>
+      </TouchableOpacity> */}
+    </View>
+    )
+};
+
+
+export default Authors;
+const styles=StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        // alignItems: "center",
+        // justifyContent: "center",
+        // marginTop:20,
+      },
+   
+   
+  card: {
+    backgroundColor: "#f0f0f0",
+    borderRadius: 100, // Half of the width and height to make an oval
+    padding: 20,
+    marginBottom: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  description: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  roundBoundary: {
+    backgroundColor: "ligtpink",
+    width: 40,
+    height: 40,
+    borderRadius: 20, // Half of the width and height to make it round
+    alignItems: "center",
+    justifyContent: "center",
+    
+  },
+  roundText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "black",
+  },
+  addButton: {
+    position: "absolute",
+    // top: 20,
+    right: 20,
+    backgroundColor: "blue",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 5,
+  },
+  addButtonText: {
+    color: "white",
+    fontSize: 16,
+  },
+})
+
---
+++ Screens/Books.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, FlatList ,Button, TextInput} from "react-native";
+import { useRoute } from "@react-navigation/native";
+
+const BooksScreen = () => {
+  const route = useRoute();
+  const { author } = route.params;
+  const[editedbooks, seteditedbooks]= useState(false);
+
+
+  const handleInputChange = () => {
+          seteditedbooks(true)
+          
+    }
+  
+    const updateBookName=(text,id)=>{
+          console.log(text,id)
+    }
+
+  return (
+    <View style={styles.container}>
+      <View  style={{flexDirection:"row"}}>
+      <Text style={styles.title}>Books for Author: {author.name}</Text> 
+      { editedbooks==false ?
+       <Button title="EDIT"  onPress={()=>handleInputChange()} style={{marginLeft: 60, alignItem: "right"}}/>
+      : <Button title="Save"  onPress={()=>seteditedbooks(false)} style={{marginLeft: 60, alignItem: "right"}}/>
+      }
+       </View>
+
+   
+      <FlatList
+        data={author.books}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.bookContainer}>
+             {editedbooks==true ? (
+               <TextInput
+               style={styles.editInput}
+               defaultValue={item.name}
+               
+               onChangeText={(text) => updateBookName( text,item.id)}
+             />
+             ):(
+              <Text style={styles.bookName}>  {item.name}  </Text>
+             )}
+           
+            
+            <Text style={styles.bookId}>ID: {item.id}</Text>
+           
+          </View>
+        )}
+      />
+      
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  bookContainer: {
+    backgroundColor: "#f0f0f0",
+    borderRadius: 8,
+    padding: 15,
+    marginBottom: 10,
+  },
+  bookName: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  bookId: {
+    fontSize: 14,
+    color: "#666",
+  },
+  editInput: {
+    fontSize: 16,
+    fontWeight: "bold",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 5,
+    marginBottom: 5,
+  },
+});
+
+export default BooksScreen;
